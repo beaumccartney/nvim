2effa9f39240171a5df66fb27928d77f5802be5f
Beau McCartney <beau@beaumccartney.xyz>
2023-03-11 11:02:10 -0700

    make find git files map more natural

--- init.lua
+++ init.lua
@@ -311,7 +311,7 @@ vim.keymap.set( "n", "cxc", "<cmd>lua require('substitute.exchange').cancel()<cr
 -- telescope maps
 local builtin = require('telescope.builtin')
 vim.keymap.set( 'n', '<leader>ff',  builtin.find_files,           {} )
-vim.keymap.set( 'n', '<leader>fg',  builtin.git_files,            {} )
+vim.keymap.set( 'n', '<leader>gg',  builtin.git_files,            {} )
 
 vim.keymap.set( 'n', '<leader>/',   builtin.live_grep,            {} )
 vim.keymap.set( 'n', '<leader>*',   builtin.grep_string,          {} )
