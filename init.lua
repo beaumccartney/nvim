9b59990cf2bfd816e7f295498cc9ccf20180826c
Beau McCartney <beau@beaumccartney.xyz>
2023-03-13 12:41:43 -0600

    remove treesitter indent
    idk what it would have done for me honestly and it makes stuff weird

--- init.lua
+++ init.lua
@@ -88,7 +88,6 @@ require'lazy'.setup {
 
             require'nvim-treesitter.configs'.setup {
                 ensure_installed      = 'all',
-                indent                = { enable = true },
                 highlight             = {
                     enable  = true,
                     disable = { 'gitcommit' },
