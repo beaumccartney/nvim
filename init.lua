cd2290b1877171fa512d12329f7207d11af280e9
Beau McCartney <mccartney.beausl@gmail.com>
2023-08-19 19:37:37 -0600

    breakindent and breakat
    - breakindent keeps wrapped text at the original indentation
    - linebreak prevents wraps halfway through a word

--- init.lua
+++ init.lua
@@ -497,12 +497,14 @@ vim.opt.termguicolors  = true
 vim.opt.nu             = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop        = 4
-vim.opt.softtabstop    = 4
-vim.opt.shiftwidth     = 4
-vim.opt.expandtab      = true
-vim.opt.smarttab       = true
-vim.opt.cindent        = true
+vim.opt.tabstop     = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth  = 4
+vim.opt.expandtab   = true
+vim.opt.smarttab    = true
+vim.opt.cindent     = true
+vim.opt.breakindent = true
+vim.opt.linebreak   = true
 
 vim.opt.scrolloff      = 10
 vim.opt.colorcolumn    = '80'
