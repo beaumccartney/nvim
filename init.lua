331af8cdbaf2eca8fa2055a026d34c8366cc32d1
Beau McCartney <mccartney.beausl@gmail.com>
2023-09-30 19:48:42 -0600

    remove my goofy paste and re-highlight stuff

--- init.lua
+++ init.lua
@@ -499,10 +499,7 @@ make_keymap( 'n', 'Y',         'y$',   opts ) -- yank to end of line
 make_keymap( 'n', '<leader>Y', '"+y$', opts ) -- yank to end of line
 
 make_keymap( { 'n', 'v' }, '<leader>y', '"+y',       opts ) -- yank to clipboard
-make_keymap( { 'n', 'v' }, '<leader>p', '"+p`]v`[]', opts ) -- put from clipboard
-
-make_keymap( 'n', 'p', "p`]v`[", opts ) -- put and reselect visual selection
-make_keymap( 'n', 'P', "P`]v`[", opts ) -- put and reselect visual selection
+make_keymap( { 'n', 'v' }, '<leader>p', '"+p', opts ) -- put from clipboard
 
 -- keep visual selection after indent
 make_keymap( 'v', '>' , '>gv', opts )
