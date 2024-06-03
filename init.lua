bc2f52c152e89ef78c3a1c5529ec6297a55a136b
Beau McCartney <mccartney.beausl@gmail.com>
2024-06-03 16:32:08 -0600

    let minicomment choose commentstring if ts context doesn't

--- init.lua
+++ init.lua
@@ -141,7 +141,7 @@ hipatterns.setup {
 require 'mini.comment'.setup {
     options = {
         custom_commentstring = function()
-            return require 'ts_context_commentstring'.calculate_commentstring() or vim.bo.commentstring
+            return require 'ts_context_commentstring'.calculate_commentstring()
         end,
     },
     mappings = { textobject = 'ic', },
