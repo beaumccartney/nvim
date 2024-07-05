0333e5513d1fa91dad293328e2fc5c874f3d97cb
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-04 19:35:05 -0600

    mini autopairs

--- init.lua
+++ init.lua
@@ -107,6 +107,19 @@ require'mini.jump'       .setup {
     }
 }
 
+require 'mini.pairs'.setup {
+    ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
+    ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
+    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+
+    [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
+    [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
+    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+
+    ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+}
+
 local miniclue = require('mini.clue')
 miniclue                 .setup {
     window = {
