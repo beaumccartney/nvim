325049b53bf94965d0719e56eb616adab037401b
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-15 08:56:31 -0700

    enable inlay hints by default

--- init.lua
+++ init.lua
@@ -819,6 +819,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         )
 
         local inlay_hint = lsp.inlay_hint
+        inlay_hint.enable(true, { bufnr = ev.buf })
         make_keymap("n", "\\H", function()
             local enabled = inlay_hint.is_enabled({ bufnr = ev.buf })
             local new = not enabled
