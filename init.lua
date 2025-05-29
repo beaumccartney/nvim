f4d3fcda5f139f438f8247f778bbca8fbf83e8af
Beau McCartney <mccartney.beausl@gmail.com>
2025-05-29 13:30:08 -0600

    remove gx alternative - comes out the box with mini

--- init.lua
+++ init.lua
@@ -201,18 +201,6 @@ miniclue.setup({
 	},
 })
 
--- gx -> <leader>gx - keep "open this thing" map and default mini exchange map
--- from https://github.com/echasnovski/mini.nvim/discussions/1009#discussioncomment-9951158
-local remap = function(mode, lhs_from, lhs_to)
-	local keymap = vim.fn.maparg(lhs_from, mode, false, true)
-	local rhs = keymap.callback or keymap.rhs
-	if rhs == nil then
-		error("Could not remap from " .. lhs_from .. " to " .. lhs_to)
-	end
-	vim.keymap.set(mode, lhs_to, rhs, { desc = keymap.desc })
-end
-remap("n", "gx", "<Leader>gx")
-remap("x", "gx", "<Leader>gx")
 require("mini.operators").setup()
 
 require("mini.trailspace").setup()
